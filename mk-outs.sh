0d40609a48bb9a08d315b1dbbed251d067b3516a
overoxidize <overoxidize@protonmail.com>
2023-01-30 17:05:16 -0500

    init commit

---
+++ mk-outs.sh
@@ -0,0 +1,9 @@
+#1/usr/bin/env zsh
+
+OUTDIR="tests/expected"
+[[ ! -d "$OUTDIR" ]] && mkdir -p "$OUTDIR"
+
+echo "Hello there" > $OUTDIR/hello1.txt
+echo "Hello" "there" > $OUTDIR/hello2.txt
+echo "Hello  there" > $OUTDIR/hello1.n.txt
+echo "Hello"   "there" > $OUTDIR/hello2.n.txt
